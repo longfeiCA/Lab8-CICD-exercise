6a20d9d880c228c983f8092631eef7ce640f9fe1
Longfei Jiao <ljiao@ualberta.ca>
2024-10-24 17:32:57 -0600

    Added a unit test for addCity function

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -8,9 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    public CustomList MockCityList() {
+        CustomList list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest() {
+        CustomList list = MockCityList();
+        int expectedCountBeforeAdd = 0;
+        assertEquals(expectedCountBeforeAdd, list.getCount(), "Count should be 0");
+        list.add(new City("edmonton", "alberta"));
+        //check count of list increase by 1
+        int expectedCountAfterAdd = 1;
+        assertEquals(expectedCountAfterAdd, list.getCount(), "After add, city count should be 1");
+    }
+
+
 
 
 }
