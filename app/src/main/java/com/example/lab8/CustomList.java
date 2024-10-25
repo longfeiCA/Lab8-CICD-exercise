5ea60de2af36b730254f5f266288839daf723b92
Longfei Jiao <ljiao@ualberta.ca>
2024-10-24 23:36:40 -0600

    fixed countCities function

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -58,7 +58,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int countCities() {
-        return 1; // intended to fail
+        return cities.size(); // intended to fail
     }
 
 }
